7aa09bbd6312ad5096f99149f3b59a2d513aa8e6
SySyAli <syedansabali786@gmail.com>
2023-12-23 02:56:55 -0500

    Fixing build.yml

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,16 +1,16 @@
 /**
  * @file main.cpp
  * @brief Test driver.
- * @detailed The test driver. Through its associated tests, this driver aims to exercise all
- * of the functionality of the AppTracker, AppEntry, and DatabaseManager classes in a clear and
- * consistent way so that the class's interface and implementation can be easily inferred.
+ * @detailed The test driver. Through its associated tests, this driver aims to
+ * exercise all of the functionality of the AppTracker, AppEntry, and
+ * DatabaseManager classes in a clear and consistent way so that the class's
+ * interface and implementation can be easily inferred.
  * @author Syed Ali
  */
 
 #include <gtest/gtest.h>
 
-int main(int argc, char** argv)
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
